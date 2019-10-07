df7ed4b9f0a5aeba2b0d0887e7dd278060b3fac4
Rhaylander Almeida <rhaylander95@gmail.com>
2019-10-07 01:08:00 -0300

    register-transaction form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { OnsenModule } from 'ngx-onsenui';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
 import { DescriptiveStatementComponent } from './descriptive-statement/descriptive-statement.component';
 import { RegisterTransactionComponent } from './register-transaction/register-transaction.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -16,6 +18,7 @@ import { RegisterTransactionComponent } from './register-transaction/register-tr
     imports: [
         BrowserModule,
         OnsenModule,
+        ReactiveFormsModule,
     ],
     schemas: [
         CUSTOM_ELEMENTS_SCHEMA,
@@ -28,3 +31,5 @@ import { RegisterTransactionComponent } from './register-transaction/register-tr
     bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
--- src/app/register-transaction/register-transaction.component.ts
+++ src/app/register-transaction/register-transaction.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'ons-page[register-transaction]',
-  templateUrl: './register-transaction.component.html',
-  styleUrls: ['./register-transaction.component.css']
+    selector: 'ons-page[register-transaction]',
+    templateUrl: './register-transaction.component.html',
+    styleUrls: ['./register-transaction.component.css']
 })
 export class RegisterTransactionComponent implements OnInit {
+    transactionTypes = [
+        { label: 'Deposit', value: 'deposit' },
+        { label: 'Expense', value: 'expense' },
+    ];
 
-  constructor() { }
+    transactionForm = this.formBuilder.group({
+        amount: [null, Validators.compose([
+            Validators.min(0),
+            Validators.required,
+        ])],
+        description: [null, Validators.required],
+        transactionType: ['', Validators.required],
+    });
 
-  ngOnInit() {
-  }
+    constructor(
+        private formBuilder: FormBuilder,
+    ) { }
 
+    ngOnInit() { }
+
+    save() {
+        console.log(this.transactionForm.value);
+    }
 }
